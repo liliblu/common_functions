144cd2275ff83ee286b4bc1cb8f61de204d7dd00
Lili Blumenberg <lili.blumenberg@gmail.com>
2019-04-09 13:08:07 -0400

    added background list option and different GO list option

--- GO_enrichment_fns.py
+++ GO_enrichment_fns.py
@@ -3,26 +3,35 @@ from scipy.stats import fisher_exact
 import pandas as pd
 import commonFns
 
-fil = '/Users/lili/google_drive/Ruggles_lab/common_functions/GO_terms/go_biological_process_gene_map.txt'
-with open(fil, 'r') as fh:
-    bio_process_go = {x.split('|')[0]: set(x.split('|')[1].split(' ')) for x in fh.readlines()}
 
-M = set()
-for v in bio_process_go.values():
-    M.update(set(v))
-M = len(M)
+def parseGOFile(fil):
+    with open(fil, 'r') as fh:
+        bio_process_go = {x.strip().split('\t')[0]: set(x.strip().split('\t', 1)[1].strip().split())for x in fh.readlines()}
 
-def calculateGOEnrichment(genes, test='hypergeom'):
+    M = set()
+    for v in bio_process_go.values():
+        M.update(set(v))
+    return bio_process_go, M
+
+def calculateGOEnrichment(genes,
+                            test='hypergeom',
+                            background_list=M,
+                            fil='/Users/lili/google_drive/Ruggles_lab/common_functions/GO_terms/GO_Biological_Process_2018.txt'):
+
+    bio_process_go, M = parseGOFile(fil)
+    M = len(M.intersection(set(background_list)))
 
     genes = set(genes)
     N = len(genes)
-    results = pd.DataFrame(columns=['GO_term', 'nGO', 'nOverlap', 'genesGO', 'genesCommon'])
+
+    results = pd.DataFrame(columns=['GO_term', 'nGO', 'nOverlap', 'genesCommon'])
     for go, lis in bio_process_go.iteritems():
+        lis = lis.intersection(background_list)
         n = len(lis)
-        k = len(lis.intersection(genes))
-        line = {'GO_term':[go],'nGO':[n], 'nOverlap':[k],
-        'genesGO':[','.join(lis)], 'genesCommon':[','.join(lis.intersection(genes))]}
-        results = results.append(pd.DataFrame.from_dict(line),sort=True, ignore_index=True)
+        if n > 0:
+            k = len(lis.intersection(genes))
+            line = {'GO_term':[go],'nGO':[n], 'nOverlap':[k], 'genesCommon':[','.join(lis.intersection(genes))]}
+            results = results.append(pd.DataFrame.from_dict(line),sort=True, ignore_index=True)
     results['pval'] = results.apply((lambda r: hypergeom.sf(k=r['nOverlap'], M=M, n=r['nGO'], N=N, loc=1)), axis=1)
     results['FDR'] = commonFns.correct_pvalues_for_multiple_testing(results['pval'])
     return results
